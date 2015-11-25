7dedcbab3066826f4e058d98ff0025708ee49ad8
Geert van Horrik <geert@catenalogic.com>
2015-11-25 19:15:03 +0100

    (*) ORCOMP-215 Prepare all libraries to support multiple languages

--- src/Orc.Wizard.Example/Orc.Wizard.Example.NET40/App.xaml.cs
+++ src/Orc.Wizard.Example/Orc.Wizard.Example.NET40/App.xaml.cs
@@ -7,8 +7,11 @@
 
 namespace Orc.Wizard.Example
 {
+    using System.Globalization;
     using System.Windows;
+    using Catel.IoC;
     using Catel.Logging;
+    using Catel.Services;
     using Catel.Windows;
 
     /// <summary>
@@ -18,11 +21,19 @@ namespace Orc.Wizard.Example
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-
 #if DEBUG
             LogManager.AddDebugListener(true);
 #endif
+
+            var languageService = ServiceLocator.Default.ResolveType<ILanguageService>();
+
+            // Note: it's best to use .CurrentUICulture in actual apps since it will use the preferred language
+            // of the user. But in order to demo multilingual features for devs (who mostly have en-US as .CurrentUICulture),
+            // we use .CurrentCulture for the sake of the demo
+            languageService.PreferredCulture = CultureInfo.CurrentCulture;
+            languageService.FallbackCulture = new CultureInfo("en-US");
+
+            base.OnStartup(e);
 
             StyleHelper.CreateStyleForwardersForDefaultStyles();
         }
