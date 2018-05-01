66327aeab80bfeed85113867c9f93b8a496e2ea6
Geert van Horrik <geert@catenalogic.com>
2018-05-01 14:27:35 +0200

    Migrate test project to new project structure

--- src/Orc.Wizard.Tests/PublicApiFacts.cs
+++ src/Orc.Wizard.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.Wizard.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_Wizard_HasNoBreakingChanges()
         {
             var assembly = typeof(WizardService).Assembly;
