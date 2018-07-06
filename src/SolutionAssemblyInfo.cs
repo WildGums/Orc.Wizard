8b707eb22cfba1e594e426ee1282b77082e99b63
Geert van Horrik <geert@catenalogic.com>
2018-07-06 14:07:05 +0200

    Update packages

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha.1")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2010 - 2018")]
 
