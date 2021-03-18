d0e7c4b3bd3859a5d4eae855e4f1f5927d247bec
Geert van Horrik <geert@catenalogic.com>
2021-03-18 09:52:05 +0100

    Add extra demo pages to show issue with scrollviewer staying at the top

---
+++ src/Orc.Wizard.Example/Wizard/ViewModels/Long2WizardPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Orc.Wizard.Example.Wizard.ViewModels
+{
+    using System.Collections.Generic;
+    using Catel.Data;
+    using Catel.MVVM;
+
+    public class Long2WizardPageViewModel : WizardPageViewModelBase<Long2WizardPage>
+    {
+        public Long2WizardPageViewModel(Long2WizardPage wizardPage)
+            : base(wizardPage)
+        {
+        }
+    }
+}
