94493b1a0e3d290f1e947bf4add878546d67a048
Geert van Horrik <geert@catenalogic.com>
2025-03-13 11:06:30 +0100

    Add IWizardExtensions.FindRequired... methods

---
+++ src/Orc.Wizard.Tests/Models/Extensions/IWizardExtensionsFacts.FindRequiredPage.cs
@@ -0,0 +1,33 @@
+﻿namespace Orc.Wizard.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Catel.IoC;
+    using NUnit.Framework;
+
+    public partial class IWizardExtensionsFacts
+    {
+        [TestFixture]
+        public class The_FindRequiredPage_Method
+        {
+            [Test]
+            public async Task Throws_Exception_When_Page_Not_Found_Async()
+            {
+                var wizard = new TestWizard(TypeFactory.Default);
+
+                Assert.Throws<InvalidOperationException>(() => wizard.FindRequiredPage(x => x.Title == "Non-Existing"));
+            }
+
+            [Test]
+            public async Task Returns_Page_Async()
+            {
+                var wizard = new TestWizard(TypeFactory.Default);
+                wizard.AddPage<TestWizardPage>();
+
+                var page = wizard.FindRequiredPage(x => x is TestWizardPage);
+
+                Assert.That(page, Is.Not.Null);
+            }
+        }
+    }
+}
---
+++ src/Orc.Wizard.Tests/Models/Extensions/IWizardExtensionsFacts.cs
@@ -0,0 +1,12 @@
+﻿namespace Orc.Wizard.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class IWizardExtensionsFacts
+    {
+    }
+}
